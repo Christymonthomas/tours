5cea369aad0a3f2ef19552498f9ff68e13e4f457
Christy <christymonthomas1999@gmail.com>
2024-05-12 17:29:23 +0530

    Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,57 @@
-const url = 'https://www.course-api.com/react-tours-project';
+import { useState, useEffect } from "react";
+const url = "https://www.course-api.com/react-tours-project";
+import Loading from "./Loading";
+import Tours from "./Tours";
 
 const App = () => {
-  return <h2>Tours Starter</h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+  const fetchTours = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setTours(tours);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">No tours left</div>
+        <button
+          type="button"
+          style={{ marginTop: "2rem" }}
+          className="btn"
+          onClick={() => fetchTours()}
+        >
+          refresh
+        </button>
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
 };
 export default App;
